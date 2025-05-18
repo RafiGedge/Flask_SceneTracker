d1220acc0697f1d2228b96040bf8824e51fb7c39
Rafi <g0534183790@gmail.com>
2025-05-18 16:12:04 +0300

    for railway

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(
@@ -7,10 +8,10 @@ app = Flask(
     template_folder="templates"
 )
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-app.run(host='0.0.0.0', port=3000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
